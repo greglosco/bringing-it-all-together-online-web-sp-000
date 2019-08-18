18ba2f2f11c45f8eb430b3ce8974df1443f46c95
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:03:42 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :id
+  
+  def initialize name:, breed:, id:
+    @id = id
+    @name = name
+    @breed = breed 
+  end
+  
+  def self.create_table
+    
+  end
+  
+end
