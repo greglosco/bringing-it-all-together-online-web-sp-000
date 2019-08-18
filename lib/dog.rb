e03fc828828a1fe702affc3d7cf816c335e6657c
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:37:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,4 +70,15 @@ class Dog
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
   
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
+  end
+  
 end
