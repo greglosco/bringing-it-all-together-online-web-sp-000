b75abf4c8ad776db75dc5def465e0a5621904af7
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:22:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,7 @@ class Dog
     
     DB[:conn].execute(sql, self.name, self.breed)
     
-    @id = DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_row_entered")
   end
   
   def self.drop_table
