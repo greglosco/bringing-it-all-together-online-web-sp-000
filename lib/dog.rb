5ef0804ac115685ddd453d0d87cbc185c9e2c10c
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:15:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    new_dog = Dog.new(name:, breed:)
+    new_dog = Dog.new(name: name, breed: breed)
     new_dog.id = row[0]
     new_dog.name = row[1]
     new_dog.breed = row[2]
