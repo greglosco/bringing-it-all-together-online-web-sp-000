119ebd51038d904da5837ff3738b97967e15314d
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:17:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,8 +33,15 @@ class Dog
     new_dog
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1 
+    SQL
     
+    DB[:conn].execute(sql)
   end
   
 end
