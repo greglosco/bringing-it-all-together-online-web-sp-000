cca3d8f0e3ef5b69e199237fe53cb45f5d68804c
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:46:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog 
   
-  attr_accessor :name, :breed, :id
+  attr_accessor :id, :name, :breed
   
   def initialize(id: nil, name:, breed:)
     @id = id
@@ -41,7 +41,7 @@ class Dog
   def update 
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    DB[:conn].execute(sql, self.id, self.name, self.breed)
   end
   
   
