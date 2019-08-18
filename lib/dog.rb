c42689a812451dfb226cdf1519e1703b9bc53c95
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:38:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,7 @@ class Dog
     DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
   end
   
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
     
   end
   
