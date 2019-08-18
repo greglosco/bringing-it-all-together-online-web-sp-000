5173bdee8ea50824c53001bebaf858ca116c4335
Gregory Losco <gtlosco@gmail.com>
2019-08-18 17:39:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,7 +84,8 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?" name, breed)
     
-    
+    if !dog.empty?
+      
   end
   
 end
